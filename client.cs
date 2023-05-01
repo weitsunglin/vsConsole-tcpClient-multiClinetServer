41fd8865b27787f3ceb74e1d9a258dd692b008e0
JAS <72340341+weitsunglin@users.noreply.github.com>
2023-05-01 14:25:02 +0800

    Add files via upload

---
+++ client.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+namespace Client{
+    class Program{
+        static int PROTOCOL = 7000;
+        static void Read(object obj){
+            TcpClient tcpClient = (TcpClient)obj; //轉型
+            StreamReader sReader = new StreamReader(tcpClient.GetStream());
+            while (true){
+                try{
+                    string message = sReader.ReadLine();
+                    Console.WriteLine(message);
+                }
+                catch (Exception e){
+                    Console.WriteLine(e.Message);
+                    break;
+                }
+            }
+        }
+        
+        static void Main(string[] args){
+
+            Random rnd = new Random();
+            int ID = rnd.Next(0, 999999);
+            TcpClient tcpClient = new TcpClient("127.0.0.1", PROTOCOL);
+            StreamWriter sWriter = new StreamWriter(tcpClient.GetStream());
+            if (tcpClient.Connected){
+                Console.WriteLine("tcpClient"+ ID.ToString()+ "Connected to server");
+                sWriter.WriteLine(ID.ToString());
+                sWriter.Flush();
+            }
+            Thread thread = new Thread(Read);
+            thread.Start(tcpClient);           
+            try{     
+                while (true){
+                    if (tcpClient.Connected){
+                        string input = Console.ReadLine();
+                        sWriter.WriteLine("clientID"+ID.ToString()+":"+ input);
+                        sWriter.Flush();
+                    }
+                }
+            }
+            catch (Exception e){
+                Console.Write(e.Message);
+            }
+            Console.ReadKey();
+            Console.WriteLine("Client close");
+        }
+    }
+}
---
+++ server.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace Server{
+    class Program{
+        static int PROTOCOL = 7000;
+        private static TcpListener tcpListener;
+        private static List<TcpClient> Clients = new List<TcpClient>();
+        static void Main(string[] args){
+            InitTcpListener();
+        }
+        public static void InitTcpListener(){
+            tcpListener = new TcpListener(IPAddress.Any, PROTOCOL);
+            tcpListener.Start();
+
+            Console.WriteLine("tcpListener start");
+            while (true){
+                TcpClient client = tcpListener.AcceptTcpClient();
+                if(client != null){
+                    StreamReader reader = new StreamReader(client.GetStream());
+                    string message = reader.ReadLine();
+                    Console.WriteLine("new client is adding ID:"+message);
+                    Clients.Add(client);
+                    Thread thread = new Thread(ClientListener);
+                    thread.Start(client);
+                    continue;
+                }
+            }
+        }
+        public static void ClientListener(object clientObj)
+        {
+            TcpClient client = (TcpClient)clientObj;
+            StreamReader reader = new StreamReader(client.GetStream());
+            while (true){
+                string message = reader.ReadLine();
+                BroadCast(message, client);
+                Console.WriteLine(message, "message from client");
+            }
+        }
+        public static void BroadCast(string msg, TcpClient excludeClient){
+            foreach (TcpClient client in Clients){
+                if (client != excludeClient){
+                    StreamWriter sWriter = new StreamWriter(client.GetStream());
+                    sWriter.WriteLine(msg);
+                    sWriter.Flush();
+                    Console.WriteLine("BroadCast to client Success");
+                }
+            }
+        }
+    }
+}
